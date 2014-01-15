68336ad4b92407315286ab56478fea3c4709cea4
Eric Wu <ewu@zynga.com>
2014-01-15 08:41:58 -0800

    Initial Commit

---
+++ TipCalculator/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  TipCalculator
+//
+//  Created by Hua Wu on 1/15/14.
+//  Copyright (c) 2014 zynga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TipCalculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TipCalculator
+//
+//  Created by Hua Wu on 1/15/14.
+//  Copyright (c) 2014 zynga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XYZAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XYZAppDelegate class]));
+    }
+}
